75f9f6502f5b1f2566753a087658372b8d8327d4
stpl-ithead <head.it@satmatechnologies.com>
2022-05-02 00:27:46 +0530

    initial import to repository

---
+++ masterdata.go
@@ -0,0 +1,64 @@
+package models
+
+type Qmeta struct {
+	Qid       int
+	Subjid    int16
+	Topicid   int16
+	Corrans   int16
+	Stdtime   int16
+	Mintime   int16
+	Lengthy   int16
+	Difflevel int16
+	Tricky    byte
+	CorrMarks float32 // extra field not exist in database
+}
+
+type Language struct {
+	ID       byte
+	Langcode string
+	Title    string
+}
+
+//type Subject struct {
+//	ID    int16
+//	Title string
+//}
+
+//type SubjAlias struct {
+//	Subjid  int16
+//	Alias string
+//}
+
+type Topic struct {
+	ID       int16
+	Subjid   int16
+	Title    string
+	Videoimg string
+}
+
+type Concept struct {
+	ID      int16
+	Subjid  int16
+	Topicid int16
+	Concept string
+}
+
+type QuesConcept struct {
+	Qid       int
+	Conceptid int16
+}
+
+type Test struct {
+	ID         int
+	Title      string
+	TestType   string
+	TotalMarks float32
+}
+
+// type TestSubject struct {
+//         testid int
+//         subjid int16
+//         aliasid int16
+//         duration int16
+//         seqno byte
+// }
---
+++ userdata.go
@@ -0,0 +1,126 @@
+package models
+
+//type UserResp struct {
+//	Qid       int
+//	Corr      byte
+//	Marks     float32
+//	Review    byte
+//	Giventime int16
+//	Timestat  byte
+//
+//	// it is not user response
+//	// it tell whether user answered the ques
+//	// or not
+//	Answered byte
+//}
+
+type UserResponse struct {
+	Qid         int
+	Seqno       int16
+	Subject     string
+	Subjid      int16
+	Topic       string
+	Correctans  string
+	Response    string
+	Timetaken   int16
+	Iscorrect   bool
+	Timestat    int8
+	Review      bool
+	Visited     bool
+	Marks       float32
+	TimestatStr string //Super/Perfect/Weak/...
+}
+
+type UserPerformance struct {
+	Testid  int
+	Userid  int
+	Subjid  int16
+	Topicid int16
+	Score   float32
+
+	//Totalques      int16
+	Answered       int16
+	Correct        int16
+	Review         int16
+	Reviewanswered int16
+	Notanswered    int16
+	Notvisited     int16
+
+	//Totaltime int16
+	Giventime int16
+	OntimeC   int16 // correct + ontime
+	OntimeW   int16 // wrong + ontime
+
+	OvertimeC int16 // correct + overtime
+	OvertimeW int16 // wrong + overtime
+
+	TooQuickC int16 // Too quick + Correct
+	TooQuickW int16 // Too quick + Wrong
+
+	CMarks float32 // total correct marks of correct-questions without subtracting negative marks of wrong questions
+	WMarks float32 // total correct marks of wrong-questions. It is sum of currect marks of those questions, instead of negative.
+
+	// Super Attempts	= OntimeC 	(correct + ontime)
+	// Perfect Attempts	= OvertimeC	(correct + overtime)
+	// Weak Attempts	= OntimeW	(wrong + ontime)
+	// Wasted Attempts 	= OvertimeW	(wrong + overtime)
+	// Guessed Attempts 	= TooQuickC	(correct + too-quick)
+	// Bogus Attempts	= TooQuickW	(wrong + too-quick)
+}
+
+type TestScore struct {
+	Testid int
+	Userid int
+	Rank   int
+	Marks  float32
+	Langid byte
+}
+
+type TestPerformance struct {
+	Testid int
+	Qid    int
+	//CorrMarks    float32 // uint16 encoded small float value
+	Tottime      int
+	Totcorrect   int
+	Totattempt   int
+	Totreview    int
+	TotrevAns    int // review and answered
+	TotontimeC   int
+	TotontimeW   int
+	TotovertimeC int
+	TotovertimeW int
+	TotquickC    int
+	TotquickW    int
+	// Totontime   int
+	// Totovertime int
+	// Totguessed  int
+	// Totwasted   int
+}
+
+type UserRespData struct {
+	TestID int
+	UserID int
+	Data   []UserResponse
+}
+
+// type QMarks struct {
+// 	Qid       int
+// 	CorrMarks float32
+// }
+
+// //GetKey - parentid1 should be Testid, parentid2 should be Userid
+// func (s UserResponse) GetKey(parentid1, parentid2 string) string {
+// 	var buffer bytes.Buffer
+// 	buffer.WriteString("RPT:RESP:TID")
+// 	buffer.WriteString(parentid1) // parentid1 should be Testid
+// 	buffer.WriteString(":UID")
+// 	buffer.WriteString(parentid2) // parentid2 should be Userid
+// 	return buffer.String()
+// }
+// func (s UserResponse) GetMasterKey() string {
+// 	return "RPT:RESP:TID<testid>:UID<userid>"
+// }
+// func (s UserResponse) GetExpiration() time.Duration {
+// 	// cache for 10 days 240 hours
+// 	return time.Duration(240) * time.Hour
+// }
